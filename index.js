25ff6db821c625261c1506134a2b72c15ebcbc47
AlaaXDz <sifodil@yandex.com>
2022-09-12 11:21:29 +0100

    Changed the Post users Api , Added the GET log

--- index.js
+++ index.js
@@ -57,40 +57,103 @@ app.post("/api/users", (req, res) => {
 })
 
 //API POST new Exercice
-app.post('/api/users/:id/exercises',(req,res)=>{
-  console.log(`req.params`,req.params)
-  const id=req.params.id;
-  const {description, duration, date} = req.body
-  User.findById(id,(err,userData=>{
-    if(err || !userData){
-      res.send("Could not find user");
-    }else{
-      const newExercice = new Exercise({
-        userId: id,
-        description,
-        duration,
-        date: new Date(date)
-      })
-      newExercice.save((err,data)=>{
-        if(err || !data){
-          res.send("There was an error saving this exercice")
-        }else{
-          const {description,duration,date,_id} = data;
-          res.json({
-            username: userData.username,
-            description,
-            duration,
-            date: date.toDateString(),
-            _id: userData.id
+app.post("/api/users/:_id/exercises",(req,res,next)=>{
+  console.log(req.body);
+  console.log(req.params);
+  console.log(req.query);
+  let userId = req.params._id
+  let description = req.body.description
+  let duration = req.body.duration
+  let date = req.body.date
+  
+  console.log(date);
 
-          })
+  if (date === "" || "undefined"){
+    date = new Date().toDateString()
+  } else {
+    date = new Date(date).toDateString()
+  }
+
+  console.log(date);
+
+  const expObj = {
+    description,
+    duration,
+    date
+  }
+
+  User.findByIdAndUpdate(
+    userId,
+    {$push:{log:expObj}},
+    {new:true},
+    (err,updatedUser)=>{
+      if(err) {
+        console.log('update error:',err);
+        return res.json('update error:', err);
+      }
+      
+      let returnObj ={
+        "_id":userId,
+        "username":updatedUser.username,
+        "date":expObj.date,
+        "duration":parseInt(expObj.duration),"description":expObj.description
+      }
+      console.log(returnObj)
+      return res.json(returnObj)
+    }
+  )  
+})
+//API GET Users
+app.get("/api/users/:id/logs", (req, res) => {
+  const { from, to, limit } = req.query;
+  const {id} = req.params;
+  User.findById(id, (err, userData) => {
+    if(err || !userData) {
+      res.send("Could not find user");
+     }else{
+      let dateObj = {}
+      if(from){
+        dateObj["$gte"] = new Date(from)
+      }
+      if(to){
+        dateObj["$lte"] = new Date(to)
+      }
+      let filter = {
+        userId: id
+      }
+      if(from || to ){
+        filter.date = dateObj
+      }
+      let nonNullLimit = limit ?? 500
+      Exercise.find(filter).limit(+nonNullLimit).exec((err, data) => {
+        if(err || !data){
+          res.json([])
+        }else{
+          const count = data.length
+          const rawLog = data
+          const {username, _id} = userData;
+          const log= rawLog.map((l) => ({
+            description: l.description,
+            duration: l.duration,
+            date: l.date.toDateString()
+          }))
+          res.json({username, count, _id, log})
         }
       })
+    } 
+  })
+})
+
+//API GET Users
+app.get("/api/users", (req, res) => {
+  User.find({}, (err, data) => {
+    if(!data){
+      res.send("No users")
+    }else{
+      res.json(data)
     }
   })
-})  
-
-
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
